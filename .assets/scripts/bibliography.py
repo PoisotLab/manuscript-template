d3f2884e847d96ba1885b747273efed33095c619
Timothée Poisot <timothee.poisot@umontreal.ca>
2020-02-07 13:28:49 -0500

    python improvement

---
+++ .assets/scripts/bibliography.py
@@ -0,0 +1,16 @@
+import subprocess
+import json
+import os
+
+with open("references.json") as jsfile:
+    references = json.load(jsfile)
+
+# Fields to remove
+fields = ["source", "abstract", "language", "accessed", "container-title-short", "title-short", "ISSN"]
+for reference in references:
+    for field in fields:
+        reference.pop(field, None)
+
+with open("references.json", "w") as outfile:
+    json.dump(references, outfile, sort_keys=True, indent=4, separators=(',', ': '))
+    outfile.write('\n')
