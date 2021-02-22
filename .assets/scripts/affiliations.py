c76cc1d697ca2d32f61ace232b2aa12321ecf8da
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-02-22 13:55:34 -0500

    ✨ biorxiv author template

--- .assets/scripts/affiliations.py
+++ .assets/scripts/affiliations.py
@@ -5,6 +5,9 @@ import os
 with open("metadata.json") as jsfile:
     metadata = json.load(jsfile)
 
+biorxiv = open("author_template.tsv", "a")
+biorxiv.write("\n")
+
 # Fields to remove
 creators = metadata["creators"]
 affiliation_to_number = {}
@@ -13,7 +16,28 @@ for creator in creators:
     affiliations = creator["affiliations"]
     for affiliation in affiliations:
         if not (affiliation in affiliation_to_number.keys()):
-            affiliation_to_number[affiliation] = len(affiliation_to_number)+1
+            affiliation_to_number[affiliation] = len(affiliation_to_number) + 1
+    # BIORXIV info block
+    if "email" in creator.keys():
+        biorxiv.write(creator["email"])
+    biorxiv.write("\t")
+    # Institution
+    biorxiv.write(affiliations[0])
+    biorxiv.write("\t")
+    # Name
+    biorxiv.write(creator["givennames"])
+    biorxiv.write("\t")
+    biorxiv.write("\t")
+    biorxiv.write(creator["familyname"])
+    biorxiv.write("\t")
+    biorxiv.write("\t")
+    biorxiv.write("\t")
+    biorxiv.write("\t")
+    biorxiv.write("\t")
+    if "orcid" in creator.keys():
+        biorxiv.write(creator["orcid"])
+    biorxiv.write("\n")
+    
 
 for creator in creators:
     creator["affilcode"] = [affiliation_to_number[affiliation]
@@ -25,7 +49,24 @@ affiliations["authors"] = creators
 affiliations["institutions"] = [{"id": v, "name": k} for k, v in sorted(
     affiliation_to_number.items(), key=lambda item: item[1])]
 
+affiliations["metadata"] = {}
+affiliations["metadata"]["corresponding"] = []
+
+# Check authorship and correspondance
+toadd = True
+for creator in affiliations["authors"]:
+    if "status" in creator.keys():
+        if "equal" in creator["status"]:
+            creator["affilcode"].append("‡")
+            if toadd:
+                affiliations["metadata"]["equalcontributions"] = {"id": "‡", "text": "These authors contributed equally to the work"}
+                toadd = False
+        if "corresponding" in creator["status"]:
+            affiliations["metadata"]["corresponding"].append({"given": creator["givennames"], "family": creator["familyname"], "email": creator["email"]})
+
 with open("affiliations.json", "w") as outfile:
     json.dump(affiliations, outfile, sort_keys=False,
               indent=4, separators=(',', ': '))
     outfile.write('\n')
+
+biorxiv.close()
