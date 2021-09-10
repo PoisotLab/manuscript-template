b69805b776d4e0db52b4c8cd4a035d49eafca679
Timothée Poisot <timothee.poisot@umontreal.ca>
2021-09-10 15:04:14 -0400

    change citation style via metadata

---
+++ .assets/scripts/citationstyle.py
@@ -0,0 +1,17 @@
+import json
+
+with open("metadata.json") as mdfile:
+    metadata = json.load(mdfile)
+
+# 
+
+from urllib import request
+
+local_file = '.assets/citationstyle.csl'
+
+try:
+    remote_url = "https://github.com/citation-style-language/styles/blob/master/%s.csl" % metadata["citationstyle"]
+    request.urlretrieve(remote_url, local_file)
+except request.HTTPError:
+    remote_url = "https://github.com/citation-style-language/styles/blob/master/dependent/%s.csl" % metadata["citationstyle"]
+    request.urlretrieve(remote_url, local_file)
